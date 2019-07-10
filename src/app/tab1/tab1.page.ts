bddbc20f9e2587bfbdba355ff0a4dfffd1706840
MrAram <avetisaram@gmail.com>
2019-07-10 16:58:42 +0400

    l

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
+  providers: [Camera],
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private camera: Camera,private domSanitizer: DomSanitizer) {}
+ images=[];
+ base: any;
+
+ nameFix(filename){
+  return filename.replace(/file:\/\//g, "")
+}
+
+
+  getPic(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.images.push(base64Image);
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  
 
 }
